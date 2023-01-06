01291183baf270f9a6c3997c981d987cdedf3cf2
Alexey Lomakin <lom2alex@gmail.com>
2023-01-06 13:18:52 +0400

    repos and service interfaces added

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,17 @@ import (
 
 	"github.com/cptobviousx/notebook"
 	"github.com/cptobviousx/notebook/pkg/handler"
+	"github.com/cptobviousx/notebook/pkg/repository"
+	"github.com/cptobviousx/notebook/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(notebook.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running server: %s", err.Error())
 	}
 }
