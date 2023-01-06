c4bf10fff519a71ad203858869360bcbbbeaa616
Alexey Lomakin <lom2alex@gmail.com>
2023-01-06 12:53:50 +0400

    [nb-01] server added

---
+++ notebook.go
@@ -0,0 +1,26 @@
+package notebook
+
+type NoteBookList struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description`
+}
+
+type UsersList struct {
+	Id     int
+	UserId int
+	ListId int
+}
+
+type NoteBookItem struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
+
+type NoteBookItemsList struct {
+	Id     int
+	ListId int
+	ItemId int
+}
